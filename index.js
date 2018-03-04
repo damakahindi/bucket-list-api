7a772c6f118b7b4632fdc62f71eaf1a7c4e15671
Esther Dama Kahindi <esther.kahindi@andela.com>
2018-03-04 13:26:17 +0300

    Disable Cors as testing for the backend

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
+
+const user = require('./server/routes/user');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-app.use(cors);
+// app.use(cors);
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://dama:dama@ds153978.mlab.com:53978/bucket-list-dama', {
-  useMongoClient: true,
-});
+mongoose.connect('mongodb://dama:dama@ds153978.mlab.com:53978/bucket-list-dama');
+
+app.use('/api', user);
 
 app.listen(port, () => {
   console.info('==> 🌎 Listening on port %s. Open up http://127.0.0.1:%s/ ', port, port);
