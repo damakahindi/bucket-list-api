aaadb2fe7b8ead7a0e848ddb4d2be1625a98bea5
Esther Dama Kahindi <esther.kahindi@andela.com>
2018-03-04 14:23:08 +0300

    Add the user googleId to be unique

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,10 +7,13 @@ const UserCtrl = {
     }
 
     const usr = new User();
-    usr.googleID = req.body.googleId;
+    usr.googleId = req.body.googleId;
 
     usr.save((error, user) => {
       if (error) {
+        if (error.code === 11000) {
+          return res.status(200).send(error.getOperation());
+        }
         return res.status(500).send(error);
       }
       return res.status(201).send(user);
