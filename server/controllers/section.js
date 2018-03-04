9090a9fba2a236081ffc5fa4a5aee771ea47750e
Esther Dama Kahindi <esther.kahindi@andela.com>
2018-03-04 14:59:09 +0300

    Add get a single section, delete and update that section. Add get sessions by the user_id

--- server/controllers/section.js
+++ server/controllers/section.js
@@ -24,6 +24,52 @@ const SectionCtrl = {
       return res.status(201).send(section);
     });
   },
+  get(req, res) {
+    Section.findById(req.params.id, (error, section) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      if (section) { return res.status(200).send(section); }
+      return res.status(404).send(section);
+    });
+  },
+  update(req, res) {
+    Section.findById({ _id: req.params.id }, (error, section) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      if (req.body.title) {
+        section.title = req.body.title;
+      }
+      if (req.body.content) {
+        section.description = req.body.description;
+      }
+      section.save((err) => {
+        if (err) {
+          return res.status(500).send(error);
+        }
+        return res.status(200).send(section);
+      });
+    });
+  },
+  getByUser(req, res) {
+    Section.find({ googleId: req.params.user_id })
+      .sort('-createdAt')
+      .exec((error, section) => {
+        if (error) {
+          return res.status(500).send(error);
+        }
+        return res.status(200).send(section);
+      });
+  },
+  delete(req, res) {
+    Section.remove({ _id: req.params.id }, (error, section) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      return res.status(200).send(section);
+    });
+  },
 };
 
 
--- server/routes/section.js
+++ server/routes/section.js
@@ -6,4 +6,12 @@ const Section = require('../controllers/section');
 router.route('/section')
   .post(Section.create);
 
+router.route('/section/:id')
+  .get(Section.get)
+  .put(Section.update)
+  .delete(Section.delete);
+
+router.route('/user/:user_id/sections')
+  .get(Section.getByUser);
+
 module.exports = router;
